25c3585f816da0c10ebe6a12d29407ebea979b28
jaybatech <139530666+jaybatech@users.noreply.github.com>
2026-01-08 16:30:19 +0530

    correction 13
    correction 13

--- server.php
+++ server.php
@@ -1,22 +1,15 @@
 <?php
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+// server.php - For PHP built-in server to serve static files
 
+if (php_sapi_name() === 'cli-server') {
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . '/public' . $url['path'];
 
- 
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+    // If requested file exists in public/, serve it directly
+    if (is_file($file)) {
+        return false;
+    }
 }
 
-require_once __DIR__.'/public/index.php';
+// Otherwise, route everything to Laravel index.php
+require __DIR__ . '/public/index.php';
