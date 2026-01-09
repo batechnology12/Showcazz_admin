38d5d2aa9a1c2aa8ed8afd2406d3406ab2647a73
jaybatech <139530666+jaybatech@users.noreply.github.com>
2026-01-09 11:46:53 +0530

    check 12
    check 12

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -57,7 +57,7 @@ class HomeController extends Controller
         })
         ->get();
 
-       dd('calling');
+       
         $activeCompanies = Company::where('is_active', 1)->get();
        // $inActiveCompanies = Company::where('is_active', 0)->get();
 
@@ -66,6 +66,7 @@ class HomeController extends Controller
         $totalFeaturedJobs = Job::where('is_featured', 1)->count();
         $totalTodaysJobs = Job::where('created_at', 'like', $today->toDateString() . '%')->count();
         $recentJobs = Job::orderBy('id', 'DESC')->take(25)->get();
+        dd('last');
         return view('admin.home')
                         ->with('totalActiveUsers', $totalActiveUsers)
                         ->with('totalVerifiedUsers', $totalVerifiedUsers)
