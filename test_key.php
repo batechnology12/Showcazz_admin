f98e785e8e6089fe92964f498f3d5ab282a5f76e
jaybatech <139530666+jaybatech@users.noreply.github.com>
2026-06-29 19:09:10 +0530

    withoutnkey

--- test_key.php
+++
@@ -1,6 +0,0 @@
-<?php
-$serviceAccount = json_decode(file_get_contents(__DIR__.'/medical-app.json'), true);
-$pk = $serviceAccount['private_key'];
-$res = openssl_pkey_get_private($pk);
-var_dump($res);
-echo openssl_error_string();
