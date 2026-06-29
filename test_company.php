a33970ec5bf95e9b665a98f6ae07b15cc4cff992
jaybatech <139530666+jaybatech@users.noreply.github.com>
2026-06-29 17:50:04 +0530

    move deploy

--- test_company.php
+++ test_company.php
@@ -12,7 +12,7 @@ try {
     $user->visibility_control = 'public';
     $user->post_visibility_control = 'public';
     $user->message_visibility_control = 'public';
-    $user->name = 'TestCompany';
+    $user->name = 'Test Company';
     $user->is_active = 1;
     $user->save();
     echo "Success!\n";
